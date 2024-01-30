ff0b68d294b46dc76b4bb9bac5d872846ecdd2c9
Riyansh Sharma <135585454+riyansh04@users.noreply.github.com>
2024-01-30 08:55:11 +0530

    Add files via upload

---
+++ test_vowels.py
@@ -0,0 +1,10 @@
+import pytest
+
+from vowels import count_vowels
+
+def test_count_vowels():
+    assert count_vowels("Hello") == 2
+    assert count_vowels("PYTHON") == 1
+    assert count_vowels("bcd") == 0
+    assert count_vowels("aeiou") == 5
+    assert count_vowels("") == "Error: No input provided."
